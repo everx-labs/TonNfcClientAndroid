5798260e50d6d0d664af4ed254d481a79e640c45
Alina T <alina1989malina@yandex.ru>
2021-02-07 23:17:47 +0300

    add CAPDUTest, RAPDUTest

---
+++ tonnfcclientandroid/src/test/java/com/tonnfccard/smartcard/wrappers/CAPDUTest.java
@@ -0,0 +1,157 @@
+package com.tonnfccard.smartcard.wrappers;
+
+import com.tonnfccard.api.utils.StringHelper;
+import com.tonnfccard.utils.ByteArrayHelper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class CAPDUTest {
+
+    private static final ByteArrayHelper BYTE_ARRAY_HELPER = ByteArrayHelper.getInstance();
+    private static final StringHelper STR_HELPER = StringHelper.getInstance();
+
+    @Test
+    public void CAPDUTest1() {
+        int cla = 0x01;
+        int ins = 0x02;
+        int p1 = 0x00;
+        int p2 = 0x03;
+        CAPDU capdu = new CAPDU(cla, ins, p1, p2);
+        assertArrayEquals(new byte[]{(byte) cla, (byte) ins, (byte) p1, (byte) p2}, capdu.getBytes());
+        assertEquals(capdu.getCla(), (byte) cla);
+        assertEquals(capdu.getIns(), (byte) ins);
+        assertEquals(capdu.getP1(), (byte) p1);
+        assertEquals(capdu.getP2(), (byte) p2);
+        assertEquals(capdu.getLc(), 0x00);
+        assertEquals(capdu.getData().length, 0x00);
+        assertEquals(capdu.getLe(), -1);
+        assertEquals(capdu.toString(), "01020003");
+    }
+
+    @Test
+    public void CAPDUTest2() {
+        int cla = 0x21;
+        int ins = 0x32;
+        int p1 = 0x01;
+        int p2 = 0x02;
+        for (int le = 0x00; le <= 0xFF; le++) {
+            CAPDU capdu = new CAPDU(cla, ins, p1, p2, le);
+            assertArrayEquals(new byte[]{(byte) cla, (byte) ins, (byte) p1, (byte) p2, (byte) le}, capdu.getBytes());
+            assertEquals(capdu.getCla(), (byte) cla);
+            assertEquals(capdu.getIns(), (byte) ins);
+            assertEquals(capdu.getP1(), (byte) p1);
+            assertEquals(capdu.getP2(), (byte) p2);
+            assertEquals(capdu.getLc(), 0x00);
+            assertEquals(capdu.getData().length, 0x00);
+            assertEquals(capdu.getLe(), le);
+            assertEquals(capdu.toString(), "21320102" + BYTE_ARRAY_HELPER.hex((byte) le));
+        }
+    }
+
+    @Test
+    public void CAPDUTest3() {
+        int cla = 0xB0;
+        int ins = 0xA1;
+        int p1 = 0x0A;
+        int p2 = 0x0B;
+        Random random = new Random();
+        for (int len = 0x01; len <= 0xFF; len++) {
+            byte[] dataField = new byte[len];
+            random.nextBytes(dataField);
+            CAPDU capdu = new CAPDU(cla, ins, p1, p2, dataField);
+            assertArrayEquals(BYTE_ARRAY_HELPER.bConcat(new byte[]{(byte) cla, (byte) ins, (byte) p1, (byte) p2, (byte) dataField.length}, dataField), capdu.getBytes());
+            assertEquals(capdu.getCla(), (byte) cla);
+            assertEquals(capdu.getIns(), (byte) ins);
+            assertEquals(capdu.getP1(), (byte) p1);
+            assertEquals(capdu.getP2(), (byte) p2);
+            assertEquals(capdu.getLc(), dataField.length);
+            assertArrayEquals(capdu.getData(), dataField);
+            assertEquals(capdu.getLe(), -1);
+            assertEquals(capdu.toString(), "B0A10A0B" + BYTE_ARRAY_HELPER.hex((byte) dataField.length) + BYTE_ARRAY_HELPER.hex(dataField));
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void CAPDUTest4() {
+        int cla = 0xB0;
+        int ins = 0xA1;
+        int p1 = 0x0A;
+        int p2 = 0x0B;
+        byte[] dataField = new byte[0];
+        new CAPDU(cla, ins, p1, p2, dataField);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void CAPDUTest5() {
+        int cla = 0xB0;
+        int ins = 0xA1;
+        int p1 = 0x0A;
+        int p2 = 0x0B;
+        new CAPDU(cla, ins, p1, p2, null);
+    }
+
+    @Test
+    public void CAPDUTest6() {
+        int cla = 0xB0;
+        int ins = 0xA1;
+        int p1 = 0x0A;
+        int p2 = 0x0B;
+        Random random = new Random();
+        for (int le = 0x00; le <= 0xFF; le++) {
+            for (int len = 0x01; len <= 0xFF; len++) {
+                byte[] dataField = new byte[len];
+                random.nextBytes(dataField);
+                CAPDU capdu = new CAPDU(cla, ins, p1, p2, dataField, le);
+                assertArrayEquals(BYTE_ARRAY_HELPER.bConcat(new byte[]{(byte) cla, (byte) ins, (byte) p1, (byte) p2, (byte) dataField.length}, dataField, new byte[]{(byte) le}), capdu.getBytes());
+                assertEquals(capdu.getCla(), (byte) cla);
+                assertEquals(capdu.getIns(), (byte) ins);
+                assertEquals(capdu.getP1(), (byte) p1);
+                assertEquals(capdu.getP2(), (byte) p2);
+                assertEquals(capdu.getLc(), dataField.length);
+                assertArrayEquals(capdu.getData(), dataField);
+                assertEquals(capdu.getLe(), le);
+                assertEquals(capdu.toString(), "B0A10A0B" + BYTE_ARRAY_HELPER.hex((byte) dataField.length) + BYTE_ARRAY_HELPER.hex(dataField) + BYTE_ARRAY_HELPER.hex((byte) le));
+            }
+        }
+    }
+
+    @Test
+    public void CAPDUTest7() {
+        int cla = 0xB0;
+        int ins = 0xA1;
+        int p1 = 0x0A;
+        int p2 = 0x0B;
+        byte[] dataField = new byte[0];
+        for (int le = 0x00; le <= 0xFF; le++) {
+            try {
+                new CAPDU(cla, ins, p1, p2, dataField, 0xFF);
+                fail();
+            } catch (IllegalArgumentException e) {
+            }
+        }
+    }
+
+    @Test
+    public void CAPDUTest8() {
+        int cla = 0xB0;
+        int ins = 0xA1;
+        int p1 = 0x0A;
+        int p2 = 0x0B;
+        for (int le = 0x00; le <= 0xFF; le++) {
+            try {
+                new CAPDU(cla, ins, p1, p2, null, 0xFF);
+                fail();
+            } catch (IllegalArgumentException e) {
+            }
+        }
+    }
+
+}
---
+++ tonnfcclientandroid/src/test/java/com/tonnfccard/smartcard/wrappers/RAPDUTest.java
@@ -0,0 +1,122 @@
+package com.tonnfccard.smartcard.wrappers;
+
+import static org.junit.Assert.*;
+
+import com.tonnfccard.api.utils.StringHelper;
+import com.tonnfccard.utils.ByteArrayHelper;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class RAPDUTest {
+
+    private static final ByteArrayHelper BYTE_ARRAY_HELPER = ByteArrayHelper.getInstance();
+    private static final StringHelper STR_HELPER = StringHelper.getInstance();
+
+    private Random random = new Random();
+
+    @Test(expected = IllegalArgumentException.class)
+    public void RAPDUTest1() {
+        byte[] data = null;
+        new RAPDU(data);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void RAPDUTest2() {
+        String data = null;
+        new RAPDU(data);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void RAPDUTest3() {
+        new RAPDU(new byte[0]);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void RAPDUTest4() {
+        new RAPDU(new byte[1]);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void RAPDUTest5() {
+        new RAPDU("");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void RAPDUTest6() {
+        new RAPDU("1");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void RAPDUTest7() {
+        new RAPDU("12");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void RAPDUTest8() {
+        new RAPDU("123");
+    }
+
+    @Test
+    public void RAPDUTest9() {
+        for (int len = 2; len <= 257; len++) {
+            byte [] bytes = new byte[len];
+            random.nextBytes(bytes);
+            RAPDU rapdu = new RAPDU(bytes);
+            assertArrayEquals(rapdu.getBytes(), bytes);
+            assertArrayEquals(rapdu.getData(), BYTE_ARRAY_HELPER.bSub(bytes, 0, len - 2));
+            assertArrayEquals(rapdu.getSW(), BYTE_ARRAY_HELPER.bSub(bytes, len - 2, 2));
+            assertEquals(rapdu.getSW1(), bytes[len - 2]);
+            assertEquals(rapdu.getSW2(), bytes[len - 1]);
+            assertEquals(rapdu.toString(), BYTE_ARRAY_HELPER.hex(BYTE_ARRAY_HELPER.bSub(bytes, len - 2, 2)) +
+                    (BYTE_ARRAY_HELPER.bSub(bytes, 0, len - 2) != null && BYTE_ARRAY_HELPER.bSub(bytes, 0, len - 2).length > 0
+                            ? " '" +   BYTE_ARRAY_HELPER.hex(BYTE_ARRAY_HELPER.bSub(bytes, 0, len - 2)) + "'"
+                            : ""));
+        }
+
+        for (int len = 4; len <= 2*257; len=len+2) {
+            String str = STR_HELPER.randomHexString(len);
+            byte [] bytes = BYTE_ARRAY_HELPER.bytes(str);
+            RAPDU rapdu = new RAPDU(str);
+            assertArrayEquals(rapdu.getBytes(), bytes);
+            assertArrayEquals(rapdu.getData(), BYTE_ARRAY_HELPER.bSub(bytes, 0, len/2 - 2));
+            assertArrayEquals(rapdu.getSW(), BYTE_ARRAY_HELPER.bSub(bytes, len/2 - 2, 2));
+            assertEquals(rapdu.getSW1(), bytes[len/2 - 2]);
+            assertEquals(rapdu.getSW2(), bytes[len/2 - 1]);
+            assertEquals(rapdu.toString(), BYTE_ARRAY_HELPER.hex(BYTE_ARRAY_HELPER.bSub(bytes, len/2 - 2, 2)) +
+                    (BYTE_ARRAY_HELPER.bSub(bytes, 0, len/2 - 2) != null && BYTE_ARRAY_HELPER.bSub(bytes, 0, len/2 - 2).length > 0
+                            ? " '" +   BYTE_ARRAY_HELPER.hex(BYTE_ARRAY_HELPER.bSub(bytes, 0, len/2 - 2)) + "'"
+                            : ""));
+        }
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void RAPDUTest10() {
+        byte [] bytes = new byte[258];
+        random.nextBytes(bytes);
+        new RAPDU(bytes);
+    }
+
+    @Test
+    public void RAPDUTest11() {
+        for (int len = 2 * 2  + 1; len <= 2 * 257 + 1; len=len+2) {
+            try{
+                new RAPDU(STR_HELPER.randomHexString(len));
+                fail();
+            }
+            catch (IllegalArgumentException e){}
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void RAPDUTest12() {
+        new RAPDU("S0ASJH");
+    }
+
+}
